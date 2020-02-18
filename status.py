148f37147d011a941d03a5dc34ba6bea9594bc79
lkm543 <lkm543@gmail.com>
2020-02-18 14:42:43 +0800

    Update status.py

--- status.py
+++ status.py
@@ -4,7 +4,7 @@ from kBar import kBar
 class status(kBar):
     coin = 'BTC'
     margin = 0.5
-    principal = 1000
+    capital = 1000
     turnOver = 0
     position = 0
     positionPrice = 0
@@ -14,20 +14,20 @@ class status(kBar):
     liquidationPrice = 0
     lastTradeTime = 0
 
-    def __init__(self, coinUsed='BTC', startPrincipal=1000):
+    def __init__(self, coinUsed='BTC', startCapital=1000):
         self.coin = coinUsed
         if coinUsed == 'BTC':
             self.margin = 0.5  # %
         else:
             self.margin = 1  # %
-        self.principal = startPrincipal
+        self.capital = startCapital
 
     # Ref:https://kknews.cc/zh-tw/finance/e4ka5ay.html
     # 维持保证金=仓位*开仓价格*2.5%=100000*0.000012*2.5%=0.03
     # 保证金余额=钱包余额-维持保证金=1-0.03=0.97
     # 假设强平价格为X，100000*（0.000012-X）=0.97，X=0.0000023
     def getLiquidationPrice(self):
-        maintainMargin = self.principal \
+        maintainMargin = self.capital \
             - self.margin * self.position / 100
         liquidationPrice = self.positionPrice \
             * (1 - maintainMargin / self.position)
@@ -43,8 +43,8 @@ class status(kBar):
         if fundingRate != lastFR:
             self.revenueTotal += abs(self.position * lastFR / 100)
             self.revenueFunding += abs(self.position * lastFR / 100)
-            amount = (self.principal + self.revenueTotal) * 0.5
-            # amount = self.principal
+            amount = (self.capital + self.revenueTotal) * 0.5
+            # amount = self.capital
             if fundingRate < 0 and self.position <= 0:
                 vwap *= (1 - priceShift)
                 if self.position < 0:
@@ -115,12 +115,25 @@ class status(kBar):
         '''
         pass
 
-        def checkLiquidation(self, kBar):
-            # liquidationPrice = self.getLiquidationPrice()
-            pass
+    def checkLiquidation(self):
+        liquidationPrice = self.getLiquidationPrice()
+        if self.position > 0 and self.low <= liquidationPrice:
+            return True
+        elif self.position < 0 and self.high >= liquidationPrice:
+            return True
+        else:
+            return False
 
-        def printStatus(self):
-            pass
+    def printStatus(self):
+        print(f'Capital: {self.capital}')
+        print(f'Total revenue: {self.revenueTotal}')
+        print(f'Unrealised PNL: {self.unrealisedPNL()}')
+        print(f'TurnOver: {self.turnOver}')
+        print(f'Last trade time: {self.lastTradeTime}')
+        print(f'Max loss: {self.maxLoss}')
+        print(f'Position: {self.position}')
+        print(f'Position Price: {self.positionPrice}')
+        print(f'Liquidation price: {self.getLiquidationPrice()}')
 
 
 if __name__ == "__main__":
