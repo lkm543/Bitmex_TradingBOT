2dcddb68f29ece185ae0b4997639c31ea4818f1d
lkm543 <lkm543@gmail.com>
2020-02-18 14:43:25 +0800

    Update backTest.py

--- backTest.py
+++ backTest.py
@@ -1,27 +1,39 @@
 import pandas as pd
 from order import order
 from crawlData import histData
-from kBar import kBar
 
 
-class backTest(order, kBar):
-    liquidation = 0
-    timestamp = ""
+class backTest(order):
+    recrawl = False
+    print_KBar = False
+
+    liquidation = False
     fundingRate = 0
-
-    def __init__(self, filename):
-        self.rawData = pd.read_csv(filename, header=0)
-        self.rawData['timestamp'] = pd.to_datetime(self.rawData['timestamp'])
 
     def strategy(self):
         if self.position < 0:
-            self.putMarketOrder(self.vwap, self.capital * 2)
+            self.putMarketOrder(self.capital * 2)
         else:
-            self.putMarketOrder(self.vwap, self.capital * -2)
+            self.putMarketOrder(self.capital * -2)
+
+    def __init__(self, dataName):
+        self.filename = dataName
+        self.print_Order = False
+
+    def readData(self):
+        self.rawData = pd.read_csv(self.filename, header=0)
+        self.rawData['timestamp'] = pd.to_datetime(self.rawData['timestamp'])
 
     def start(self):
+        if self.recrawl:
+            history = histData()
+            history.complement("BTC", "BTC.csv")
+            # history.complement("ETH", "ETH.csv")
+        print(f"Start to read data from {self.filename}...")
+        self.readData()
+        print(f"Start to back test your strategy...")
         for i in range(0, len(self.rawData)):
-            # Step 0: Get k bar this minute
+            # Step 0: Get k bar of this minute
             self.fundingRate = self.rawData.loc[i, 'fundingRate']
             self.open = self.rawData.loc[i, 'open']
             self.close = self.rawData.loc[i, 'close']
@@ -30,6 +42,8 @@ class backTest(order, kBar):
             self.vwap = self.rawData.loc[i, 'vwap']
             self.volume = self.rawData.loc[i, 'volume']
             self.timestamp = self.rawData.loc[i, 'timestamp']
+            if self.print_KBar:
+                self.printKBar()
 
             # Step 1: Calculate funding
             if False:
@@ -45,17 +59,16 @@ class backTest(order, kBar):
 
             # Step 4: Execute trading strategy
             self.strategy()
+
         if self.liquidation:
             print("Your strategy was liquidated.")
-            print("The result of your strategy is:")
-            self.printStatus()
+        else:
+            print("The back test of your strategy was finished.")
+        print("The result of your strategy is:")
+        self.printStatus()
 
 
 if __name__ == '__main__':
     # TODO: VWAP=NA?
-    history = histData()
-    history.complement("BTC", "BTC.csv")
-    # history.complement("ETH", "ETH.csv")
-
     test = backTest("BTC.csv")
     test.start()
