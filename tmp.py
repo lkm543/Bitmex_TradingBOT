8df603652c648fd7ebe8c5103f92a43121e44511
lkm543 <lkm543@gmail.com>
2020-02-19 16:36:14 +0800

    delete files

--- tmp.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('BTC.csv', header=None)
-df.rename(columns={0: 'timestamp', 1: 'symbol',2:'open',3:'high',4:'low',5:'close',6:'trades',7:'volume',8:'vwap',9:'lastSize',10:'turnover',11:'homeNotional',12:'foreignNotional'}, inplace=True)
-df.to_csv('BTCwithCname.csv', index=False) # save to new csv file
